f17f7d4b47cf0391281f24e55aaf52c217853f51
pie-suhee <120539985+pie-suhee@users.noreply.github.com>
2023-10-13 13:43:42 +0900

    20231013

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header>
+            <div className="logo">
+                
+            </div>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/views/Main.js
@@ -0,0 +1,10 @@
+const Main = () => {
+    return (
+        <section className='main'>  
+            <section className='contents_con'>
+            </section>
+        </section>
+    );
+};
+
+export default Main;
