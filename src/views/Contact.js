b1e712047da1915d380e0986200b949157446907
pie-suhee <120539985+pie-suhee@users.noreply.github.com>
2023-10-20 16:23:11 +0900

    20231020

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,12 +1,18 @@
+import React, { useState } from 'react';
 import '../scss/contact.scss';
 
 const Contact = () => {
+    const letters = [...'CONTACT'];
+
     return (  
         <section className="contact">
             <div className="subTitle">
-                <span>
-                    CONTACT
-                </span>
+                {letters.map((letter, index) => (
+                    <span 
+                        id={`spell${index}`}
+                        className="spell"
+                    >{letter}</span>
+                ))}
             </div>
         </section>
     );
