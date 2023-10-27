86ed559afa6f13b12d57732ea6cebe42c8ee2198
pie-suhee <120539985+pie-suhee@users.noreply.github.com>
2023-10-27 16:14:38 +0900

    20231027

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../scss/contact.scss';
 
 const Contact = () => {
-    const letters = [...'CONTACT'];
+    const [activeItem, setActiveItem] = useState(null);
+    
+    const handleCopyClipBoard = async (text, index) => {
+        try {
+            await navigator.clipboard.writeText(text);
+            setActiveItem(index);
+            setTimeout(() => setActiveItem(null), 700);
+        } catch (e) {
+            alert('복사에 실패하였습니다');
+        }
+    };
 
     return (  
         <section className="contact">
             <div className="subTitle">
-                {letters.map((letter, index) => (
-                    <span 
-                        id={`spell${index}`}
-                        className="spell"
-                    >
-                        {letter}
-                    </span>
-                ))}
+                <span className="text">
+                    CONTACT
+                </span>
+            </div>
+
+            <div className="copyList">
+                <ul>
+                    <li className={`call ${activeItem === 0 ? 'click' : ''}`} onClick={() => {handleCopyClipBoard('01072272783', 0)}}>
+                        <img src="/icon/phoneIcon.png" alt="전화번호"/>
+                    </li>
+                    <li className={`mail ${activeItem === 1 ? 'click' : ''}`} onClick={() => {handleCopyClipBoard('suheeim314@gmail.com', 1)}}>
+                        <img src="/icon/mailIcon.png" alt="메일주소"/>
+                    </li>
+                    <li className={`adress ${activeItem === 2 ? 'click' : ''}`} onClick={() => {handleCopyClipBoard('서울특별시 송파구 오금동', 2)}}>
+                        <img src="/icon/adressIcon.png" alt="집주소"/>
+                    </li>
+                    <li className="github">
+                        <a href="https://github.com/pie-suhee" target="_blank" rel="noreferrer">
+                            <img src="/icon/githubIcon.png" alt="github"/>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </section>
     );
