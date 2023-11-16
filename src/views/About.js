84dd4a50117fe0a2a004ff6a225e9fe43bf20309
pie-suhee <120539985+pie-suhee@users.noreply.github.com>
2023-11-16 16:16:40 +0900

    20231116

--- src/views/About.js
+++ src/views/About.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from "react-router-dom";
 
 import '../scss/about.scss'; 
 import Greeting from '../components/Greeting';
@@ -6,6 +7,12 @@ import Skill from '../components/Skill';
 import Show from '../components/Show';
 
 const About = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [location]);
+
     return (
         <section className="page about">
             <Greeting />
